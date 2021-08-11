4703e384af7d67bd5def295ef77ee0af235fe19a
Irina <ir1nak0z@yandex.ru>
2021-08-11 21:46:27 +0300

    add

---
+++ WinFormsApp2/Texture.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+namespace WinFormsApp2
+{
+    public enum TextureType { Sky, Ground, ETL };
+    class Texture
+    {
+        protected TextureType Tt;
+        protected TextureBrush Tb;
+    }
+}
