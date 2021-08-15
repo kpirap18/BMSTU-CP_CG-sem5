8f005d1a72844d1e0004d4c9ba7940ca5a178941
Irina <ir1nak0z@yandex.ru>
2021-08-15 22:14:18 +0300

    h

---
+++ WinFormsApp/Shadow.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms.DataVisualization.Charting;
+
+
+namespace WinFormsApp
+{
+    class Shadow
+    {
+        public Shadow(Point3D cen, Point3D p, House h, double y)
+        {
+
+        }
+
+        public void DrawShadow(Graphics g)
+        {
+
+        }
+        private readonly PointF[][] _sh;
+    }
+}
